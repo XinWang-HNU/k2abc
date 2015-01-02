69e739aed5d7cd617927d66a58ce684fe48b5da4
mijung <brients@gmail.com>
2015-01-02 17:40:11 +0000

    testabc

--- runcode/testabc.m
+++ runcode/testabc.m
@@ -1,3 +1,33 @@
 %% test abc
 
 
+%% (1) generate data
+
+
+
+%% (2) draw parameters from the prior (theta_j)
+% e.g., fix sigma, and draw mean from a Gaussian
+
+
+%% (3) sample y from the parameters (y_i^j)
+
+
+
+
+%% (4) compute MMD for each y_i^j and y*_i^j
+
+
+
+%% (5) compute w_j which gives us posterior mean of theta
+
+
+
+
+%% (6) compute f(sigma, epsilon) = squared distance between theta_mean and theta_true
+
+
+
+%% to do: 
+
+
+
