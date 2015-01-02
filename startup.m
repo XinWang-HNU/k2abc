29156e62867b626528dc5f30f10769a10e5b62e3
wittawat <wittawatj@gmail.com>
2015-01-02 17:48:51 +0000

    updated startup.m file

---
+++ startup.m
@@ -0,0 +1,20 @@
+% Matlab startup file.
+% Include necessary dependencies
+base = pwd();
+fs = filesep();
+addpath(pwd);
+
+% folders to be added by genpath
+gfolders = {'mainfolder', 'runcode'};
+for i=1:length(gfolders)
+    fol = gfolders{i};
+    p = [base , fs, fol];
+    fprintf('add gen path: %s\n', p);
+    addpath(genpath(p));
+end
+
+% addpath(genpath(fullfile(base, 'real')));
+% addpath(genpath(fullfile(base, 'other')));
+
+clear base fs gfolders i fol p 
+
