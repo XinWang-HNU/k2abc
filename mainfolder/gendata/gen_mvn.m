bd9268e4c8f23cfaf721ca16a31814b47a8e4f98
mijung <brients@gmail.com>
2015-01-02 18:40:15 +0000

    generate data

--- mainfolder/gendata/gen_mvn.m
+++ mainfolder/gendata/gen_mvn.m
@@ -1,1 +1,13 @@
 % generate data from a multivariate normal distribution 
+
+function dat = gen_mvn(mu, sig, howmanysamps)
+
+% inputs
+%         mu: mean
+%         sig: covariance
+%         howmanysamps: how many samples to generate
+%
+% outputs
+%        dat: data
+
+dat = mvnrnd(mu, sig, howmanysamps);
