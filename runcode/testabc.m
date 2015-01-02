a18f2c48ffe4b56eb0eaa87409b6f9a9a662f2de
mijung <brients@gmail.com>
2015-01-02 19:44:32 +0000

    edit to see how epsilon affects

--- runcode/testabc.m
+++ runcode/testabc.m
@@ -22,50 +22,60 @@ theta_samps = zeros(M, 1);
 
 % these will vary later
 kernelparams = meddistance(yobs)^2;
-epsilon = 1; 
+howmanyepsilon = 5;
+epsilon = logspace(-4, 10, howmanyepsilon);
+% epsilon = 1000; 
 
-% we sample y L times, where each y consists of Ns samples
-L = 50;
-Ns = 200;
-k = zeros(M, L);
+muhat = zeros(howmanyepsilon,1);
 
-for j=1:M
+for count = 1:howmanyepsilon
     
-    %% (2) draw parameters from the prior (theta_j)
-    % e.g., fix sigma, and draw mean from a Gaussian
+    % we sample y L times, where each y consists of Ns samples
+    L = 50;
+    Ns = 200;
+    k = zeros(M, L);
     
-    theta_samps(j) = mvnrnd(zeros(1, d)+7, prior_var);
-    
-    %% (3) sample y from the parameters (y_i^j)
-    
-    for l = 1:L
+    for j=1:M
         
-        [j l]
+        %% (2) draw parameters from the prior (theta_j)
+        % e.g., fix sigma, and draw mean from a Gaussian
         
-        y = gen_mvn(theta_samps(j), theta_var, Ns);
+        theta_samps(j) = mvnrnd(zeros(1, d)+10, prior_var);
         
-        %% (4) compute MMD for each y_i^j and y*_i^j
-        ker = KGaussian(kernelparams); 
-        k(j, l) = exp(-mmd(y, yobs, ker)^2/epsilon);
+        %% (3) sample y from the parameters (y_i^j)
+        
+        parfor l = 1:L
+            
+            [count j l]
+            
+            y = gen_mvn(theta_samps(j), theta_var, Ns);
+            
+            %% (4) compute MMD for each y_i^j and y*_i^j
+            ker = KGaussian(kernelparams);
+            k(j, l) = exp(-mmd(y, yobs, ker)^2/epsilon(count));
+            
+        end
         
     end
     
+    %% (5) compute w_j which gives us posterior mean of theta
+    
+    wj_numerator = sum(k, 2)/L;
+    wj_denominator = sum(sum(k))/L;
+    
+    muhat(count) = sum(wj_numerator.*theta_samps)/wj_denominator;
+    
+    [theta_mean muhat(count)]
+    
 end
-
-%% (5) compute w_j which gives us posterior mean of theta
-
-wj_numerator = sum(k, 2)/L;
-wj_denominator = sum(sum(k));
-
-muhat = sum((wj_numerator.*theta_samps)/wj_denominator);
-
-[theta_mean muhat]
 
 %% (6) compute f(sigma, epsilon) = squared distance between theta_mean and theta_true
 
+mse = @(a) (a-theta_mean).^2;
+subplot(211); plot(muhat)
+subplot(212); plot(mse(muhat))
 
 
-%% to do:
 
 
 
