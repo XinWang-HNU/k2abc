093f3dcac8c43cac190f7c0a3d924998d29d888b
mijung <brients@gmail.com>
2015-01-21 19:05:39 +0000

    fixed seed to oldRng

---
+++ mainfolder/gendata/gen_sigmoid_pw_const.m
@@ -0,0 +1,29 @@
+% generate data from a piecewise sigmoid likelihood function
+
+function dat = gen_sigmoid_pw_const(params, n, seed)
+
+% inputs
+%         params: theta
+%         n: number of observations
+%         seed: 
+%
+% outputs
+%        dat: 
+%           (1) dat.samps: samples
+%           (2) dat.probs: true probabilities
+
+if nargin < 4
+    seed = 1;
+end
+
+oldRng = rng();
+rng(seed);
+
+% generate data
+[ samples, probs ] = like_sigmoid_pw_const( params, n );
+
+% output
+dat.samps = samples;
+dat.probs = probs;
+
+rng(oldRng);
