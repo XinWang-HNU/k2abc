5e40a5ddbcadaa3630c0e803ad6342d5ef6f1bf4
Wittawat Jitkrittum <wittawatj@gmail.com>
2015-01-21 11:03:00 +0000

    add comment to demo_ssf_kernel.abc.m

--- runcode/demo_ssf_kernel_abc.m
+++ runcode/demo_ssf_kernel_abc.m
@@ -11,15 +11,22 @@ num_obs = 200;
 obs = likelihood_func(true_theta, num_obs );
 
 
-% options 
+% options. All options are described in ssf_kernel_abc.
 op = struct();
 op.seed = seed;
+% A proposal distribution for drawing the latent variables of interest.
+% func_handle : n -> (d' x n) where n is the number of samples to draw.
+% Return a d' x n matrix.
 % 0-mean Gaussian proposal
 op.proposal_dist = @(n)randn(1, n)*sqrt(5);
+% Likelihood function handle. func : (theta, n) -> (d'' x n) where theta is one 
+% drawn latent vector and n is the number of samples to draw.
 % Gaussian likelihood 
 op.likelihood_func = likelihood_func;
 op.epsilon_list = logspace(-2, 0, 6);
+% number of latent variables (i.e., theta) of interest to draw
 op.num_latent_draws = 200;
+% number of pseudo data to draw e.g., the data drawn from the likelihood function
 op.num_pseudo_data = 200;
 
 % width squared.
@@ -28,6 +35,8 @@ op.mmd_kernel = KGaussian(width2);
 op.mmd_exponent = 2;
 
 [R, op] = ssf_kernel_abc(obs, op);
+% R contains latent samples and their weights for each epsilon.
+%
 figure 
 hold on 
 cols = 3;
@@ -38,7 +47,7 @@ for ei = 1:num_eps
     % plot empirical distribution of the drawn latent
     stem(R.latent_samples, R.norm_weights(:, ei));
     set(gca, 'fontsize', 16);
-    title(sprintf('ep = %.3f ', ep ));
+    title(sprintf('ep = %.2g ', ep ));
     grid on 
 
 end
