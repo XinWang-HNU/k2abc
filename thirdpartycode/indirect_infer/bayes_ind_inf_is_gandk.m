902338fdba0d01cae0a08fc7ec45fc3f4419269f
mijung <brients@gmail.com>
2015-06-03 15:38:15 +0200

    added comments so that I can use this code

--- thirdpartycode/indirect_infer/bayes_ind_inf_is_gandk.m
+++ thirdpartycode/indirect_infer/bayes_ind_inf_is_gandk.m
@@ -1,15 +1,30 @@
 function [theta summary_stats_samp abc_discs] = bayes_ind_inf_is_gandk(y,M,n,cov_rw,abc_tol,numComp)
 
+%% Christopher Drovandi's code 
+
+%% mijung comments : input arguments seem like
+% (1) y: true observation, size(y) = n by 1
+% (2) M : number of posterior samples of parameters (theta)
+% (3) n:  number of true observations
+% (4) cov_rw: prior covariance, size(cov_rw) = number params by num params
+% (5) abc_tol: tolerence for discrepancy between log datalikelihood given auxiliary model
+% (6) numComp: number of components for mixture model for auxiliary model
+
+%% mijung comments : size(theta) is how many samples by number of parameters
 theta = zeros(M,4);
+
+%% mijung comments : theta_curr is prior mean and cov_rw is prior cov, and numComp: how many mixture models to use
 
 % MH - IL
 theta_curr = [3,1.02,2.09,0.48];
 
+%%
 abc_discs = zeros(M,1);
 abc_disc = abc_tol;
 summary_stats_samp = zeros(M,3*numComp-1);
 summary_stats = zeros(1,3*numComp-1);
 
+% mijung comments: fitting data to gaussian mixture
 obj = gmdistribution.fit(y,numComp,'Options',statset('MaxIter',100000,'TolFun',1e-10));
 
 theta_d = [obj.PComponents(1:(numComp-1)) obj.mu' reshape(obj.Sigma,numComp,1)'];
@@ -32,7 +47,10 @@ for i = 1:M
         abc_discs(i) = abc_disc;
         continue;
     end
+    
+    % mijung comments : here y_s is simulated data, size(y_s) = n by 1
     y_s = simulate_gk(n,[theta_prop(1:2) 0.8 theta_prop(3:4)]);
+    
     %[grad,~,~] = gradest(@(x)log_like_mixture(x,y_s),theta_d);
     grad = compute_grad(theta_d,y_s,obj,numComp);
     dist_prop = grad*weight_matrix*grad';
@@ -78,7 +96,8 @@ function the_grad = compute_grad(theta,y,obj,numComp)
     end
     
 function W = compute_obs_inf(theta,y,obj,numComp)
-        
+% mijung comments: this seems like computing the information matrix
+% J(\phi((y))
         
 w = theta(1:(numComp-1));
 w(numComp) = 1-sum(theta(1:(numComp-1)));
