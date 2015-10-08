1a7a456cb4a6791a8adc33da2e0f9452a6a2759d
mijung <brients@gmail.com>
2015-10-08 12:02:54 +0100

    k2abc and sl took the previous results

--- blowfly/boxplot_for_mmd_approximations.m
+++ blowfly/boxplot_for_mmd_approximations.m
@@ -1,6 +1,9 @@
 % to show mses on sufficient stats to compare mmds
 % mijung wrote on oct 6, 2015
 
+clear all;
+clc;
+
 oldRng = rng();
 seed = 2;
 rng(seed);
@@ -10,7 +13,7 @@ load flydata.mat
 n = length(flydata);
 
 num_rept_mse = 100;
-how_many_methods = 4;
+how_many_methods = 2;
 msemat = zeros(num_rept_mse, how_many_methods);
 s = ss_for_blowflydata(flydata);
 mse = @(a) norm(s-a);
@@ -19,13 +22,13 @@ for i=1:num_rept_mse
     
     %% k2 abc (quadratic mmd)
     
-    whichmethod = 'ssf_kernel_abc';
-    minIdx1= 11;
-    minIdx2 = 2;
-    load(strcat('blowflydata: ', num2str(whichmethod), 'fromXV', '.mat'), 'results');
-    params_ours = results.post_mean(minIdx2,:);
-    
-    simuldat_ours = gendata_pop_dyn_eqn(params_ours, n);
+%     whichmethod = 'ssf_kernel_abc';
+%     minIdx1= 11;
+%     minIdx2 = 2;
+%     load(strcat('blowflydata: ', num2str(whichmethod), 'fromXV', '.mat'), 'results');
+%     params_ours = results.post_mean(minIdx2,:);
+%     
+%     simuldat_ours = gendata_pop_dyn_eqn(params_ours, n);
     
     %% ours  with different MMD estimators
     
@@ -36,23 +39,30 @@ for i=1:num_rept_mse
     simuldat_ours_lin = gendata_pop_dyn_eqn(opt_k2abc_lin, n);
     
     %% synthetic likelihood abc
-    load thetas_sl_ep_point1.mat % accpt rate is 0.26
-    params_sl = mean(thetas);
-    params_sl = [exp(params_sl(1:5)) params_sl(end)];
-    simuldat_sl = gendata_pop_dyn_eqn(params_sl, n);
+%     load thetas_sl_ep_point1.mat % accpt rate is 0.26
+%     params_sl = mean(thetas);
+%     params_sl = [exp(params_sl(1:5)) params_sl(end)];
+%     simuldat_sl = gendata_pop_dyn_eqn(params_sl, n);
    
     %% compute chosen summary statistics
-    s_ours =  ss_for_blowflydata(simuldat_ours);
+%     s_ours =  ss_for_blowflydata(simuldat_ours);
     s_ours_rf =  ss_for_blowflydata(simuldat_ours_rf);
     s_ours_lin =  ss_for_blowflydata(simuldat_ours_lin);
-    s_sl = ss_for_blowflydata(simuldat_sl);
+%     s_sl = ss_for_blowflydata(simuldat_sl);
     
-    msemat(i,:) = [mse(s_ours) mse(s_ours_rf) mse(s_ours_lin)  mse(s_sl) ];
+    msemat(i,:) = [mse(s_ours_rf) mse(s_ours_lin)];
     
 end
 
-mean(msemat)
-std(msemat)
+% mean(msemat)
+% std(msemat)
+
+%%
+
+load mse_k2abc;
+load mse_sl; 
+
+msemat = [mse_k2abc msemat mse_sl];
 
 %%
 
